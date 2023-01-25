97288aa3b67643398c2e6653304ded945d6ac7c8
JunkyByte <adriano.donninelli@hotmail.it>
2023-01-25 15:35:12 +0100

    wip

--- src/grid_window.py
+++ src/grid_window.py
@@ -4,25 +4,47 @@ import rpack
 
 
 class GridWindow_2:
-    def __init__(self, max_width=1280, max_height=960):
+    def __init__(self, max_width, max_height, autoscale=True):
         self.name = 'autogrid'
         cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(self.name, max_width, max_height)
-        self._waitkey = 1
         self.max_width = max_width
         self.max_height = max_height
+        self.autoscale = autoscale
+
+        self._waitkey = 1
+        self.s = 1
         self.last_sizes = None
         self.positions = None
 
-    def update(self, images):
-        if not isinstance(images, list):
-            images = [images]
-
+    def update(self, images: list):
         sizes = [img.shape[:2][::-1] for img in images]
-        if self.last_sizes == None or not sizes == self.last_sizes:
-            self.positions = rpack.pack(sizes, max_width=self.max_width, max_height=self.max_height)
-            self.last_sizes = sizes
-        max_x, max_y = rpack.bbox_size(sizes, self.positions)
+        source_change = self.last_sizes != sizes
+        self.last_sizes = sizes
+        print('Change', source_change)
+        if source_change:
+            self.s = 1
+
+        while True:  # Find a way to represent the images
+            try:
+                if self.s != 1:
+                    print('Rescaling with ratio', self.s)
+                    images = [cv2.resize(img, (int(s[0] * self.s), int(s[1] * self.s)), cv2.INTER_LINEAR)
+                              for s, img in zip(sizes, images)]
+                curr_sizes = [img.shape[:2][::-1] for img in images]
+                if source_change:
+                    print('packing')
+                    self.positions = rpack.pack(curr_sizes, max_width=self.max_width, max_height=self.max_height)
+                    print('finished packing!')
+                max_x, max_y = rpack.bbox_size(curr_sizes, self.positions)
+                cv2.resizeWindow(self.name, max_x, max_y)
+                break
+            except rpack.PackingImpossibleError as e:
+                if not self.autoscale:
+                    raise e
+                self.s *= 4 / 5
+                # TODO: This can ofc be more efficient
+                # (especially at start -> scale so that highest is < max_height)
 
         window = np.zeros((max_y, max_x, 3), dtype=np.uint8)
         for img, (x, y) in zip(images, self.positions):
@@ -31,57 +53,11 @@ class GridWindow_2:
         cv2.imshow(self.name, window)
         return cv2.waitKey(self._waitkey)
 
-class GridWindow:
-    def __init__(self, n, names=None, max_width=1280, max_height=960):
-        self.name = 'grid window'
-        cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(self.name, max_width, max_height)
-
-        self.grid_size = n ** (1 / 2)
-        if self.grid_size != int(self.grid_size):
-            self.grid_size = self.grid_size + 1
-        self.grid_size = int(self.grid_size)
-
-        self.width = max_width // self.grid_size
-        self.height = max_height // self.grid_size
-
-        self.names = names
-        self._waitkey = 1
-        self.indices = np.mgrid[0:self.grid_size, 0:self.grid_size].T.reshape((-1, 2))
-
-        if n == 2:
-            self.window = np.zeros((max_height // 2, max_width, 3), dtype=np.uint8)
-        else:
-            self.window = np.zeros((max_height, max_width, 3), dtype=np.uint8)
-
-    def update(self, images):
-        if not isinstance(images, list):
-            images = [images]
-        assert self.names is None or len(images) == len(self.names)
-
-        for ith, (j, i) in enumerate(self.indices):
-            if ith >= len(images):
-                break
-
-            ys, ye = i * self.height, (i + 1) * self.height
-            xs, xe = j * self.width, (j + 1) * self.width
-            frame = cv2.resize(images[ith], (self.width, self.height), cv2.INTER_LINEAR)
-
-            self.window[ys:ye, xs:xe] = frame
-
-            if self.names is not None:
-                cv2.putText(self.window, str(self.names[ith]).upper(), (xs + 5, ys + 30), 1, 2, (255, 0, 0), 0, cv2.LINE_AA)
-
-        cv2.imshow(self.name, self.window)
-        return cv2.waitKey(self._waitkey)
-
 
 if __name__ == '__main__':
     import glob
-    n = 4
-    names = ['N' + str(i) for i in range(n)]
-    # w = GridWindow(n, names=names, max_width=640, max_height=640)
-    w = GridWindow_2(max_width=1024, max_height=640)
+    import itertools
+    w = GridWindow_2(max_width=1024, max_height=768)
 
     videos = glob.glob('../samples/*.mp4')
     frames = []
@@ -95,10 +71,13 @@ if __name__ == '__main__':
         frames.append(fs)
         print(f'Video sample with {len(fs)} frames')
 
-    w._waitkey = 50
-    ss = np.random.randint(120, 320, size=n * 2)
-    sizes = [(ss[i], ss[i + 1]) for i in range(0, len(ss), 2)]
-    for frame in frames:
-        frames = [cv2.resize(frame, s, cv2.INTER_LINEAR) for s in sizes]
-        if w.update(frames) & 0xFF == ord('q'):
+    w._waitkey = 30
+    for ith, frame in enumerate(itertools.zip_longest(*frames, fillvalue=None)):
+        if any(f is None for f in frame):
+            break
+        if ith % 10 == 0:
+            ss = np.random.randint(240, 640, size=len(videos) * 2)
+            sizes = [(ss[i], ss[i + 1]) for i in range(0, len(ss), 2)]
+        frame = [cv2.resize(f, s, cv2.INTER_LINEAR) for (f, s) in zip(frame, sizes)]
+        if w.update(frame) & 0xFF == ord('q'):
             break
